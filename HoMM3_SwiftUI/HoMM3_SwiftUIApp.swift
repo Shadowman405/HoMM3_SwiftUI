4203531a39740bb7d20cfb7cc34d301b09764b05
m_mitin <maximus405@gmail.com>
2022-08-27 14:44:58 +0300

    Initial Commit

---
+++ HoMM3_SwiftUI/HoMM3_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoMM3_SwiftUIApp.swift
+//  HoMM3_SwiftUI
+//
+//  Created by Maxim Mitin on 27.08.22.
+//
+
+import SwiftUI
+
+@main
+struct HoMM3_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HoMM3_SwiftUIUITests/HoMM3_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HoMM3_SwiftUIUITestsLaunchTests.swift
+//  HoMM3_SwiftUIUITests
+//
+//  Created by Maxim Mitin on 27.08.22.
+//
+
+import XCTest
+
+class HoMM3_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
