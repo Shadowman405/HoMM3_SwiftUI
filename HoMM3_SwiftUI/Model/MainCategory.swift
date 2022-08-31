430b80225505a607d98720d528a2510d22444d75
m_mitin <maximus405@gmail.com>
2022-08-31 23:28:08 +0300

    add some spell data

--- HoMM3_SwiftUI/Model/MainCategory.swift
+++ HoMM3_SwiftUI/Model/MainCategory.swift
@@ -114,7 +114,8 @@ func getAllSpells() -> [Spell] {
     let spellsArray = [
         Spell(id: 0, name: "Bloodlust", imageName: "Bloodlust_small", description: "Increases the attack skill of target (or all) allied creature for melee attacks.",spellLevel: 1, spellSchool: .FireMagic),
         Spell(id: 1, name: "Cure", imageName: "Cure_small", description: "Removes all negative spell effects from target (or all) allied troop and heals it for (10/20/30 + (power x 5)) health points.", spellLevel: 1, spellSchool: .WaterMagic),
-        Spell(id: 2, name: "Curse", imageName: "Curse_small", description: "The targeted (or all) enemy unit(s) deliver minimum or reduced damage when attacking.", spellLevel: 1, spellSchool: .FireMagic)
+        Spell(id: 2, name: "Curse", imageName: "Curse_small", description: "The targeted (or all) enemy unit(s) deliver minimum or reduced damage when attacking.", spellLevel: 1, spellSchool: .FireMagic),
+        Spell(id: 3, name: "Dispel", imageName: "Dispel_small", description: "Removes all spell effects from target allied, enemy or all creatures and the battlefield.", spellLevel: 1, spellSchool: .WaterMagic)
     ]
     
     return spellsArray
