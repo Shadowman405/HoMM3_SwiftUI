f30bad54a2421fce666baa1bc1e7566f8db8e762
m_mitin <maximus405@gmail.com>
2022-09-02 21:45:27 +0300

    adding VideoView

---
+++ HoMM3_SwiftUI/View/VideoPlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  VideoPlayerView.swift
+//  HoMM3_SwiftUI
+//
+//  Created by Maxim Mitin on 2.09.22.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoPlayerView()
+    }
+}
