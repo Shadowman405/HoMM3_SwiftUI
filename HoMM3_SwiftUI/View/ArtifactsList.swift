59a1e07d77a3bfd89d1ae5e3e7b76bc845a79918
m_mitin <maximus405@gmail.com>
2022-09-02 20:39:25 +0300

    Configure Artifactlistview

--- HoMM3_SwiftUI/View/ArtifactsList.swift
+++ HoMM3_SwiftUI/View/ArtifactsList.swift
@@ -12,7 +12,13 @@ struct ArtifactsList: View {
     
     var body: some View {
         List {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            ForEach(mainCat.artifacts){ artifact in
+                HStack {
+                    Image(artifact.imageName)
+                    Text(artifact.name)
+                        .font(.title2)
+                }
+            }
         }
     }
 }
