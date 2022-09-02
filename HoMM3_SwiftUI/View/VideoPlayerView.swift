6455582128ca338fef538f4884e681ce8e393665
m_mitin <maximus405@gmail.com>
2022-09-02 22:26:52 +0300

    adding Loop Player

--- HoMM3_SwiftUI/View/VideoPlayerView.swift
+++ HoMM3_SwiftUI/View/VideoPlayerView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 import AVKit
 
-struct VideoPlayerView: View {
-    var body: some View {
-        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "H3main_video", withExtension: "mp4")!))
+struct VideoPlayerView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> UIView {
+        return PlauerUIView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        //
+    }
+}
+
+class PlauerUIView: UIView {
+    private var playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+        //Refs to video
+        let fileUrl = Bundle.main.url(forResource: "H3main_video", withExtension: "mp4")!
+        let playerItem = AVPlayerItem(url: fileUrl)
+        
+        
+        //Player set
+        let player = AVQueuePlayer(playerItem: playerItem)
+        playerLayer.player = player
+        playerLayer.videoGravity = .resize
+        layer.addSublayer(playerLayer)
+        
+        //Loop
+        playerLooper = AVPlayerLooper(player: player, templateItem: playerItem)
+        
+        //Play
+        player.play()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
