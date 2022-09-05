b1c2fef5eb609b718ba9d35cee419e05b75fda24
m_mitin <maximus405@gmail.com>
2022-09-05 21:37:53 +0300

    adding animated back

---
+++ HoMM3_SwiftUI/View/AnimatedBackView.swift
@@ -0,0 +1,42 @@
+//
+//  AnimatedBackView.swift
+//  HoMM3_SwiftUI
+//
+//  Created by Maxim Mitin on 5.09.22.
+//
+
+import SwiftUI
+
+struct AnimatedBackView: View {
+    var body: some View {
+        AnimatedBack()
+            .edgesIgnoringSafeArea(.all)
+            .blur(radius: 50)
+    }
+}
+
+struct AnimatedBack: View {
+    @State var start = UnitPoint(x: 0, y: -2)
+    @State var end = UnitPoint(x: 4, y: 0)
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .default).autoconnect()
+    let colors = [Color.blue , Color.red, Color.purple, Color.pink, Color.yellow, Color.green, Color.orange]
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: colors), startPoint: start, endPoint: end)
+            .animation(Animation.easeInOut(duration: 6)
+                .repeatForever())
+            .onReceive(timer) { _ in
+                self.start = UnitPoint(x: 4, y: 0)
+                self.end = UnitPoint(x: 0, y: -2)
+                self.start = UnitPoint(x: -4, y: 20)
+                self.start = UnitPoint(x: 4, y: 0)
+            }
+    }
+}
+
+struct AnimatedBackView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatedBackView()
+    }
+}
