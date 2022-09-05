47a8d37955d531d7d43f7f6367fca0cb10bfa88c
m_mitin <maximus405@gmail.com>
2022-09-05 21:56:38 +0300

    working on Artifact list adding some style

--- HoMM3_SwiftUI/View/ArtifactsList.swift
+++ HoMM3_SwiftUI/View/ArtifactsList.swift
@@ -17,7 +17,18 @@ struct ArtifactsList: View {
             ForEach(searchResults){ artifact in
                 ZStack(alignment:.leading) {
                     HStack{
-                        Image(artifact.imageName)
+                        ZStack {
+                            Circle().foregroundColor(.indigo)
+
+                            
+                            Image(artifact.imageName)
+                                .clipShape(Circle().stroke(lineWidth: 80))
+                            .shadow(color: .orange, radius: 10)
+                        }
+                        .frame(width: 50, height: 50)
+                        
+                        
+                        
                         Text(artifact.name)
                             .font(.title2)
                     }
